39a05ab82da4d7bf9bbea9d975dd77f24ad7d917
Bryson <bryson@Brysons-Laptop.local>
2024-06-04 15:47:08 -0700

    Initialize rulemapping feature to map inputs and outputs from rules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,13 @@ import { RuleMappingService } from './api/ruleMapping/ruleMapping.service';
     MongooseModule.forRoot(process.env.MONGODB_URL),
     MongooseModule.forFeature([{ name: RuleData.name, schema: RuleDataSchema }]),
   ],
-  controllers: [RuleDataController, DecisionsController, DocumentsController, SubmissionsController],
-  providers: [RuleDataService, DecisionsService, DocumentsService, SubmissionsService],
+  controllers: [
+    RuleDataController,
+    DecisionsController,
+    DocumentsController,
+    SubmissionsController,
+    RuleMappingController,
+  ],
+  providers: [RuleDataService, DecisionsService, DocumentsService, SubmissionsService, RuleMappingService],
 })
 export class AppModule {}
