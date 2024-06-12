35df3713166bee08e001b626232d06586b1f8b90
brysonjbest <bryson@Brysons-Laptop.local>
2024-06-12 15:56:59 -0700

    Revert classes to interfaces, and export classes directly in DTO.

--- src/api/ruleMapping/dto/evaluate-rulemapping.dto.ts
+++ src/api/ruleMapping/dto/evaluate-rulemapping.dto.ts
@@ -1,6 +1,57 @@
 import { IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
-import { Edge, Node, TraceObject } from '../ruleMapping.interface';
+import { Edge, Node, TraceObject, TraceObjectEntry } from '../ruleMapping.interface';
+
+export class EdgeClass implements Edge {
+  id: string;
+  type: string;
+  targetId: string;
+  sourceId: string;
+  sourceHandle?: string;
+  targetHandle?: string;
+
+  constructor(
+    id: string,
+    type: string,
+    targetId: string,
+    sourceId: string,
+    sourceHandle?: string,
+    targetHandle?: string,
+  ) {
+    this.id = id;
+    this.type = type;
+    this.targetId = targetId;
+    this.sourceId = sourceId;
+    this.sourceHandle = sourceHandle;
+    this.targetHandle = targetHandle;
+  }
+}
+
+export class TraceObjectEntryClass implements TraceObjectEntry {
+  id: string;
+  name: string;
+  input: any;
+  output: any;
+  performance?: string;
+  traceData?: any;
+
+  constructor(id: string, name: string, input: any, output: any, performance?: string, traceData?: any) {
+    this.id = id;
+    this.name = name;
+    this.input = input;
+    this.output = output;
+    this.performance = performance;
+    this.traceData = traceData;
+  }
+}
+
+export class TraceObjectClass implements TraceObject {
+  [key: string]: TraceObjectEntryClass;
+
+  constructor(entries: { [key: string]: TraceObjectEntryClass }) {
+    Object.assign(this, entries);
+  }
+}
 
 export class EvaluateRuleMappingDto {
   @IsArray()
@@ -10,12 +61,12 @@ export class EvaluateRuleMappingDto {
 
   @IsArray()
   @ValidateNested({ each: true })
-  @Type(() => Edge)
+  @Type(() => EdgeClass)
   edges: Edge[];
 }
 
 export class EvaluateRuleRunSchemaDto {
   @ValidateNested()
-  @Type(() => TraceObject)
+  @Type(() => TraceObjectClass)
   trace: TraceObject;
 }
--- src/api/ruleMapping/ruleMapping.interface.ts
+++ src/api/ruleMapping/ruleMapping.interface.ts
@@ -1,112 +1,51 @@
 import { ZenEngineTrace } from '@gorules/zen-engine';
 
-export class Field {
+export interface Field {
   id: string;
   name: string;
   type: string;
   field: string;
-
-  constructor(id: string, name: string, type: string, field: string) {
-    this.id = id;
-    this.name = name;
-    this.type = type;
-    this.field = field;
-  }
 }
 
-export class InputField extends Field {
-  constructor(id: string, name: string, type: string, field: string) {
-    super(id, name, type, field);
-  }
-}
+export interface InputField extends Field {}
 
-export class OutputField extends Field {
-  constructor(id: string, name: string, type: string, field: string) {
-    super(id, name, type, field);
-  }
-}
+export interface OutputField extends Field {}
 
-export class Expression {
+export interface Expression {
   key: string;
   value: string;
-
-  constructor(key: string, value: string) {
-    this.key = key;
-    this.value = value;
-  }
 }
 
-export class NodeContent {
+export interface NodeContent {
   inputs?: InputField[];
   outputs?: OutputField[];
   expressions?: Expression[];
-
-  constructor(inputs?: InputField[], outputs?: OutputField[], expressions?: Expression[]) {
-    this.inputs = inputs || [];
-    this.outputs = outputs || [];
-    this.expressions = expressions || [];
-  }
 }
 
-export class Node {
+export interface Node {
   id: any;
   type: string;
   content: NodeContent;
-
-  constructor(id: any, type: string, content: NodeContent) {
-    this.id = id;
-    this.type = type;
-    this.content = content;
-  }
 }
 
-export class Edge {
+export interface Edge {
   id: string;
   type: string;
   targetId: string;
   sourceId: string;
   sourceHandle?: string;
   targetHandle?: string;
-
-  constructor(
-    id: string,
-    type: string,
-    targetId: string,
-    sourceId: string,
-    sourceHandle?: string,
-    targetHandle?: string,
-  ) {
-    this.id = id;
-    this.type = type;
-    this.targetId = targetId;
-    this.sourceId = sourceId;
-    this.sourceHandle = sourceHandle;
-    this.targetHandle = targetHandle;
-  }
 }
 
-export class TraceObjectEntry implements ZenEngineTrace {
+export interface TraceObjectEntry extends ZenEngineTrace {
   id: string;
   name: string;
   input: any;
   output: any;
   performance?: string;
   traceData?: any;
-
-  constructor(id: string, name: string, input: any, output: any, performance?: string, traceData?: any) {
-    this.id = id;
-    this.name = name;
-    this.input = input;
-    this.output = output;
-    this.performance = performance;
-    this.traceData = traceData;
-  }
 }
 
-export class TraceObject {
+export interface TraceObject {
   [key: string]: TraceObjectEntry;
-
-  constructor(entries: { [key: string]: TraceObjectEntry }) {
-    Object.assign(this, entries);
-  }
 }
